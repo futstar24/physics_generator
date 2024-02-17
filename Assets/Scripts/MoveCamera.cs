5120540730b26657582d6051d5e82c6519aad132
pk0st <pkostyukov@binghamton.edu>
2024-02-17 15:37:58 -0500

    p

--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -7,12 +7,26 @@ public class MoveCamera : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        { //wheel goes up
+            if (Camera.main.transform.position.y >= 0)
+            {
+                Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, 0, -10);
+            }
+            else
+            {
+                Camera.main.transform.position = new Vector3(Camera.main.transform.position.x + 0, Camera.main.transform.position.y + 0.1f, -10);
+            }
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            Camera.main.transform.position = new Vector3(Camera.main.transform.position.x + 0, Camera.main.transform.position.y +  - 0.1f, -10);
+        }
     }
 }
